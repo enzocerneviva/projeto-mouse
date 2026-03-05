7c959b505cc15604c99263f9b86246e374e918a2
enzocerneviva <contcerneviva@gmail.com>
2026-03-04 23:55:31 -0300

    Inicialização do projeto no github - Aula 02 / 03

---
+++ src/br/com/projetomouse/main/Main.java
@@ -0,0 +1,37 @@
+package br.com.projetomouse.main;
+
+import br.com.projetomouse.model.Mouse;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("-----------------------------------------");
+        System.out.println("------ Inicializando Projeto Mouse ------");
+        System.out.println("-----------------------------------------");
+
+        Mouse mouse1 = new Mouse();
+
+        mouse1.setDPI(0);
+        mouse1.setPesoEmGramas(20);
+
+        mouse1.setDPI(800);
+        mouse1.setCor("Vermelho");
+        mouse1.setPesoEmGramas(100);
+
+        mouse1.setCor("verde");
+        mouse1.setPesoEmGramas(60);
+
+        mouse1.deslocarPonteiro(720, 480);
+        mouse1.deslocarPonteiro(200, 400);
+
+        mouse1.scrollar(2);
+        mouse1.scrollar(-3);
+        mouse1.scrollar(0);
+
+        System.out.println("\nCaracterísticas do Mouse: " +
+                "\nCor: " + mouse1.getCor() +
+                "\nDPI: " + mouse1.getDPI() +
+                "\nPeso em Gramas: " + mouse1.getDPI());
+    }
+}
---
+++ src/br/com/projetomouse/model/Mouse.java
@@ -0,0 +1,91 @@
+package br.com.projetomouse.model;
+
+public class Mouse {
+    public long DPI;
+    public String cor;
+    public double pesoEmGramas;
+    private double xAtual = 0;
+    private double yAtual = 0;
+    private int pixelsPorScroll = 100;
+    private boolean corDefinida = false;
+    private boolean pesoDefinido = false;
+
+    public void deslocarPonteiro (final double x, final double y) {
+        System.out.println("\nDeslocando ponteiro...");
+
+        System.out.println("\nCoordenada anterior: (" + this.xAtual + ", " + this.yAtual + ")");
+
+        double distanciaDeslocada = Math.sqrt(Math.pow((y - this.yAtual), 2) + Math.pow((x - this.xAtual), 2));
+        double centimetrosDeslocados = (distanciaDeslocada / this.DPI) * 2.54; // 2.54 equivale a quantidade de centím. de uma polegada.
+        this.xAtual = x;
+        this.yAtual = y;
+
+        System.out.println("Coordenada atual: (" + x + ", " + y + ")");
+        System.out.println("Distância deslocada em pixels: " + distanciaDeslocada);
+        System.out.println("Deslocamento do mouse em cm: " + centimetrosDeslocados);
+
+        System.out.println(" ");
+    }
+
+    public void scrollar (int passosScroll) {
+
+        System.out.println("\nVerificando deslocamento com scroll...\n");
+
+        int deslocamentoEmPixelsScroll = passosScroll * this.pixelsPorScroll;
+
+        if (passosScroll > 0) {
+            System.out.println("Deslocamento de " + deslocamentoEmPixelsScroll + " para cima!");
+        } else if (passosScroll < 0) {
+            System.out.println("Deslocamento de " + (-1 * deslocamentoEmPixelsScroll) + " para baixo!");
+        } else {
+            System.out.println("Não houve deslocamento utilizando o scroll do mouse!");
+        }
+
+        System.out.println(" ");
+
+    }
+
+    public void setCor(String cor) {
+        if (corDefinida == false) {
+            this.cor = cor;
+            this.corDefinida = true;
+        } else {
+            System.out.println("A cor já do mouse já foi definida anteriormente");
+        }
+
+    }
+
+    // Mantive o DPI alterável mais de uma vez porque tem mouses que possuem essa opção
+    public void setDPI(long DPI) {
+        if (DPI > 0) {
+            this.DPI = DPI;
+        } else {
+            System.out.println("Esse valor não é válido para ser atribuido ao DPI");
+        }
+    }
+
+    public void setPesoEmGramas(double pesoEmGramas) {
+        if (pesoDefinido == false) {
+            if (pesoEmGramas >= 30) {
+                this.pesoEmGramas = pesoEmGramas;
+                this.pesoDefinido = true;
+            } else {
+                System.out.println("Esse peso não é válido para o mouse");
+            }
+        } else {
+            System.out.println("O peso(g) já do mouse já foi definida anteriormente");
+        }
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public long getDPI() {
+        return DPI;
+    }
+
+    public double getPesoEmGramas() {
+        return pesoEmGramas;
+    }
+}
